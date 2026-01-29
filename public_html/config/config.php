3668457cd6b45aad6b412110e28eb4089889cba6
chufachek <153504187+chufachek@users.noreply.github.com>
2026-01-30 00:57:28 +0500

    Change database host from 127.0.0.1 to localhost

--- public_html/config/config.php
+++ public_html/config/config.php
@@ -29,7 +29,7 @@ loadEnv(BASE_PATH . '/.env');
 
 $config = array(
     'db' => array(
-        'host' => getenv('DB_HOST') ? getenv('DB_HOST') : '127.0.0.1',
+        'host' => getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost',
         'name' => getenv('DB_NAME') ? getenv('DB_NAME') : 'nikolxz1_3',
         'user' => getenv('DB_USER') ? getenv('DB_USER') : 'nikolxz1_3',
         'pass' => getenv('DB_PASS') ? getenv('DB_PASS') : 'Qwerty228;',
